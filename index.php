95faeecfd41871b74a57b95c3eb77a149e502905
Gvidas <gvisis@gmail.com>
2021-02-17 18:10:20 +0200

    task2

--- index.php
+++ index.php
@@ -21,7 +21,8 @@
           $arrayTask1[$key1][$key2] = rand(5,25);
         }
       }
-    _dc($arrayTask1);
+    echo '<pre>';
+    print_r($arrayTask1);
     ?>
   </div>
   <div class="task2">
@@ -34,8 +35,60 @@
           if ($value > 10) $countOver10++;
         }
       }
+    echo "<h3> -------- A --------- </h3>";
     echo "Count over 10: $countOver10";
+
+    echo "<h3> -------- B --------- </h3>";
+    $maxValue = 0;
+
+    foreach($arrayTask1 as $arrayInside){
+      if($maxValue <= max($arrayInside)){
+        $maxValue = max($arrayInside);
+      };
+    }
+    echo "Max value in the array: $maxValue";
+
+    echo "<h3> -------- C --------- </h3>";
+    $totalSum = [];
+
+    foreach($arrayTask1 as $arrayInside) {
+      foreach($arrayInside as $key => $value) {
+        if (!isset($totalSum[$key])) {
+          $totalSum[$key] = 0;
+        }
+        $totalSum[$key] += $value;
+      }
+    }
+    echo 'Total sums of indexes: <pre>';
+    print_r($totalSum);
+    echo '</pre>';
+
+    echo "<h3> -------- D --------- </h3>";
+    
+    foreach($arrayTask1 as $key1 => $arrayInside){
+      foreach(range(1,7) as $key2 => $_) {
+        if ($key2 >= 5){
+          $arrayTask1[$key1][] = rand(2,25);
+        }
+      }
+  }
+  echo '<pre>';
+  print_r($arrayTask1);
+  echo '</pre>';
+
+    echo "<h3> -------- E --------- </h3>";
+
+    /* 
+    Suskaičiuokite kiekvieno iš antro lygio masyvų elementų sumą atskirai ir sumas panaudokite kaip reikšmes sukuriant naują masyvą. T.y. pirma naujo masyvo reikšmė turi būti lygi mažesnio masyvo, turinčio indeksą 0 dideliame masyve, visų elementų sumai 
+    */
+    $arrayTask2E = [];
+    foreach($arrayTask1 as $arrayInside){
+      $arrayTask2E[] = array_sum($arrayInside) . '<br>';
+    }
+    echo '<pre>';
+    print_r($arrayTask2E);
     ?>
+
   </div>
   <div class="task3">
     <?php
